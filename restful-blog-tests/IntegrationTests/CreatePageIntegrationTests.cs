8f27cbe04c72ab33b8e67a9cae607aabf7ba63ca
Dominik P <46871088+nixxcode@users.noreply.github.com>
2019-09-04 02:53:32 +0100

    [Test] Added integration tests for Create and Index blog pages

--- restful-blog-tests/IntegrationTests/CreatePageIntegrationTests.cs
+++ restful-blog-tests/IntegrationTests/CreatePageIntegrationTests.cs
@@ -16,11 +16,11 @@ using Xunit;
 
 namespace restful_blog_tests.Tests
 {
-    public class IntegrationTests : IClassFixture<WebApplicationFactory<Startup>>
+    public class CreatePageIntegrationTests : IClassFixture<WebApplicationFactory<Startup>>
     {
         private HttpClient client;
 
-        public IntegrationTests(WebApplicationFactory<Startup> factory)
+        public CreatePageIntegrationTests(WebApplicationFactory<Startup> factory)
         {
             client = factory.CreateClient(new WebApplicationFactoryClientOptions
             {
@@ -29,14 +29,33 @@ namespace restful_blog_tests.Tests
         }
 
         [Fact]
-        public async void submitting_valid_blog_post_redirects_to_index()
+        public async void Get_CreatePage_ReturnsOK()
+        {
+            var response = await client.GetAsync("/blog/create");
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+
+        // Anti forgery cookie and validation token not present. Should fail no matter what we actually POST
+        [Fact]
+        public async void Post_CreatePage_InvalidRequest_ReturnsBadRequest()
+        {
+            var content = new FormUrlEncodedContent(new Dictionary<string, string>()
+            {
+                ["Title"] = "Some Title",
+                ["Content"] = "Some Content" 
+            });
+            var response = await client.PostAsync("/blog/create", content);
+            Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+        }
+
+        [Fact]
+        public async void Post_CreatePage_ValidBlogPost_ReturnsRedirectToIndex()
         {
             /*  GET the "create" page first, just like a web browser would. 
                 We need this for the validation token and anti-forgery cookie */
             var defaultPage = await client.GetAsync("/blog/create");
             var content = await HtmlHelpers.GetDocumentAsync(defaultPage);
 
-            // Act
             var response = await client.SendAsync(
                 (IHtmlFormElement)content.QuerySelector("form"),
                 (IHtmlInputElement)content.QuerySelector("input[type='submit']"),
@@ -46,21 +65,19 @@ namespace restful_blog_tests.Tests
                     ["BlogPost.Content"] = "Some Content"
                 });
 
-
             Assert.Equal(HttpStatusCode.OK, defaultPage.StatusCode);
             Assert.Equal(HttpStatusCode.Redirect, response.StatusCode);
             Assert.Equal("/Blog", response.Headers.Location.OriginalString);
         }
 
         [Fact]
-        public async void submitting_invalid_blog_post_doesnt_redirect()
+        public async void Post_CreatePage_InvalidBlogPost_ReturnsOK()
         {
             /*  GET the "create" page first, just like a web browser would. 
                 We need this for the validation token and anti-forgery cookie */
             var defaultPage = await client.GetAsync("/blog/create");
             var content = await HtmlHelpers.GetDocumentAsync(defaultPage);
 
-            // Act
             var response = await client.SendAsync(
                 (IHtmlFormElement)content.QuerySelector("form"),
                 (IHtmlInputElement)content.QuerySelector("input[type='submit']"),
@@ -69,7 +86,6 @@ namespace restful_blog_tests.Tests
                     // We define only title, no content. This makes the post invalid
                     ["BlogPost.Title"] = "Some Title"
                 });
-
 
             Assert.Equal(HttpStatusCode.OK, defaultPage.StatusCode);
             Assert.Equal(HttpStatusCode.OK, response.StatusCode);
